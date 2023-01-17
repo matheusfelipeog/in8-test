f1185aa38f28abb0a82c798baca7483288a6d70f
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-01-17 09:20:07 -0300

    Create setup and teardown function to playwright

---
+++ scraper/config.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+
+from playwright.sync_api import Browser, BrowserContext, Page, Playwright
+
+from scraper.consts import BASE_URL
+
+
+def setup_playwright(
+    playwright: Playwright
+) -> Tuple[Browser, BrowserContext, Page]:
+
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context(base_url=BASE_URL)
+    page = context.new_page()
+
+    return (browser, context, page)
+
+
+def teardown_playwright(
+    browser: Browser,
+    context: BrowserContext,
+    page: Page
+) -> None:
+
+    page.close()
+    context.close()
+    browser.close()
