f47d679175fa88f8950e22a51cb84afc72e4d03a
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-01-18 00:35:56 -0300

    Config CORS and add flask-cors in requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from flask_cors import CORS
+
 from app import api
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    CORS(app)
 
     @app.route('/')
     def index():
